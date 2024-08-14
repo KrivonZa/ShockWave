7b92759f87e18d208e320ac9e5e24ddea6908229
KevinTruong <kevintruong6587@gmail.com>
2024-08-14 16:40:54 +0700

    Fix header and design layout for homepage

--- src/component/componentTemplate/HeaderHome.jsx
+++ src/component/componentTemplate/HeaderHome.jsx
@@ -7,13 +7,12 @@ export const HeaderHome = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-between items-center text-[var(--primary-text-color)] text-xl font-medium px-4 py-4 bg-gradient-to-r from-[#1e0c37] via-purple-950 to-[#0E001A] border-b-2 border-b-purple-950">
-      <div className="flex items-center space-x-20 px-20">
+    <div className="sticky top-0 z-50 flex justify-between items-center text-[var(--primary-text-color)] text-xl font-medium px-4 py-3 bg-gradient-to-r from-[#003687] via-[#7000e0] to-[#003687] border-b-2 border-b-[#003687] shadow-[0px_4px_10px_0px_rgba(0,54,135,0.4),0px_6px_20px_0px_rgba(112,0,224,0.4),0px_8px_30px_0px_rgba(0,54,135,0.4)]">
+      <div className="flex items-center space-x-12 px-20">
         <div className="h-20 w-20 rounded-full cursor-pointer" onClick={() => navigate('/home')}>
           <img src="/image/ShockWave.png" alt="Logo"></img>
         </div>
         <div className="hover:shadow-glow duration-200 cursor-pointer" onClick={() => navigate('/home')}>Home</div>
-        <div className="hover:shadow-glow duration-200 cursor-pointer" onClick={() => navigate('/about')}>About</div>
         <div className="hover:shadow-glow duration-200 cursor-pointer" onClick={() => navigate('/contact')}>Contact</div>
       </div>
 
--- src/componentPage/Home/Homepage.jsx
+++ src/componentPage/Home/Homepage.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, {useEffect} from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const Homepage = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // animation duration
+      once: true, // whether animation should happen only once
+    });
+  }, []);
+
   return (
-    <div className="text-[var(--primary-text-color)] my-10 mx-36">
-      <div className="text-5xl font-semibold text-center">ShockWave</div>
-      <div className="font-normal text-center mt-4">Unleash the Power of Instant Messaging with ShockWave – Where Speed Meets Versatility</div>
+    <div className="text-[var(--primary-text-color)] my-10 mx-36 text-lg">
+      <div className="flex items-center justify-center h-[65vh] flex-col" data-aos="fade-up">
+        <div className="text-5xl font-semibold text-center">ShockWave</div>
+        <div className="font-normal text-center mt-4">Unleash the Power of Instant Messaging with ShockWave – Where Speed Meets Versatility</div>
+      </div>
+
+      {/* 1 hình ảnh + một câu slogan và 1 câu miêu tả, sinh động thêm*/}
+      <div className="mx-2 mt-32">
+        {/* Giới thiệu sơ lược về ShockWave */}
+        <div className="grid grid-flow-col grid-cols-2 gap-x-6 h-[65vh] p-6">
+          <div className="flex flex-col justify-center" data-aos="fade-right">
+            <div className="text-4xl font-extrabold mb-4">About ShockWave</div>
+            <div className="leading-relaxed">Shockwave is your go-to platform for seamless real-time communication. With an intuitive design and powerful features, it makes chatting with friends, family, or colleagues fast and easy. Enjoy instant messaging, group chats, media sharing, and more, all from any device.</div>
+          </div>
+          <div className="flex items-center justify-center" data-aos="fade-left">
+            <img src='/ads/About-1.png' alt='A girl smiling while chatting on the phone' className="rounded-lg object-cover h-full" />
+          </div>
+        </div>
+
+        {/* Tính năng của ShockWave */}
+        {/* Nhanh chóng, tiện lợi */}
+        <div></div>
+
+        {/* Đầy đủ kho emoji và gif */}
+        <div></div>
+
+        {/* Có thể tự custom chủ đề cho từng đoạn chat riêng biệt */}
+        <div></div>
+
+        {/* Bảo mật thông tin người dùng */}
+        <div></div>
+
+        {/* Cập nhật thường xuyên dựa vào nhu cầu người dùng và xu hướng thế giới */}
+        <div></div>
+
+        {/* Đăng kí */}
+        <div></div>
+
+        {/* Thắc mắc và liên hệ */}
+        <div></div>
+
+      </div>
     </div>
   );
 };
