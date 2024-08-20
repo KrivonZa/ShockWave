14379e6c418d522036015843f6d1fc1c5873a1b7
KevinTruong <kevintruong6587@gmail.com>
2024-08-20 22:15:45 +0700

    Re design layout

--- src/component/componentTemplate/HeaderHome.jsx
+++ src/component/componentTemplate/HeaderHome.jsx
@@ -7,7 +7,7 @@ export const HeaderHome = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="sticky top-0 z-50 flex justify-between items-center text-[var(--primary-text-color)] text-xl font-medium px-4 py-3 bg-gradient-to-r from-[#003687] via-[#7000e0] to-[#003687] border-b-2 border-b-[#003687] shadow-[0px_4px_10px_0px_rgba(0,54,135,0.4),0px_6px_20px_0px_rgba(112,0,224,0.4),0px_8px_30px_0px_rgba(0,54,135,0.4)]">
+    <div className="sticky top-0 z-50 flex justify-between items-center text-[var(--primary-text-color)] text-xl font-medium px-4 py-3 bg-[#F7FBFC] bg-opacity-75 backdrop-blur-sm border-b-2 border-b-[#B9D7EA] shadow-[0px_8px_20px_5px_rgba(44,116,179,0.4)]">
       <div className="flex items-center space-x-12 px-20">
         <div className="h-20 w-20 rounded-full cursor-pointer" onClick={() => navigate('/home')}>
           <img src="/image/ShockWave.png" alt="Logo"></img>
--- src/componentPage/Home/Homepage.jsx
+++ src/componentPage/Home/Homepage.jsx
@@ -1,6 +1,7 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Button } from "@mui/material";
 
 export const Homepage = () => {
 
@@ -21,7 +22,7 @@ export const Homepage = () => {
       {/* 1 hình ảnh + một câu slogan và 1 câu miêu tả, sinh động thêm*/}
       <div className="mx-2 mt-32">
         {/* Giới thiệu sơ lược về ShockWave */}
-        <div className="grid grid-flow-col grid-cols-2 gap-x-6 h-[65vh] p-6">
+        <div className="grid grid-flow-col grid-cols-2 gap-x-6 h-[65vh] p-6 my-32">
           <div className="flex flex-col justify-center" data-aos="fade-right">
             <div className="text-4xl font-extrabold mb-4">About ShockWave</div>
             <div className="leading-relaxed">Shockwave is your go-to platform for seamless real-time communication. With an intuitive design and powerful features, it makes chatting with friends, family, or colleagues fast and easy. Enjoy instant messaging, group chats, media sharing, and more, all from any device.</div>
@@ -32,33 +33,34 @@ export const Homepage = () => {
         </div>
 
         {/* Tính năng của ShockWave */}
-        <div className="text-center text-5xl font-extrabold">Feature</div>
         {/* Nhanh chóng, tiện lợi */}
-        <div className="grid grid-flow-col grid-cols-2 gap-x-6 h-[65vh] p-6">
-          <div>
-            <div className="text-4xl font-extrabold mb-4">Fast and Convenient</div>
-            <div>Shockwave offers lightning-fast, convenient messaging at your fingertips. Experience quick, easy navigation with an intuitive interface designed for seamless communication. Whether you're chatting in groups or one-on-one, Shockwave ensures a smooth and responsive experience every time.</div>
+        <div className="grid grid-flow-col grid-cols-2 gap-x-6 h-[65vh] p-6 my-32">
+          <div className="flex items-center justify-center" data-aos="fade-right">
+            <img src='/ads/About-2.png' alt='An example of how fast that a message is sending' className="rounded-lg object-cover h-full" />
+          </div>
+          <div className="flex flex-col justify-center" data-aos="fade-left">
+            <div className="text-4xl font-extrabold mb-4">Key Feature</div>
+            <div className="leading-relaxed">Shockwave offers lightning-fast messaging, a vast library of emojis and GIFs, customizable chat themes, robust user data security, and regular updates to stay current with user needs and global trends.</div>
           </div>
         </div>
 
-        {/* Đầy đủ kho emoji và gif */}
-        <div></div>
-
-        {/* Có thể tự custom chủ đề cho từng đoạn chat riêng biệt */}
-        <div></div>
-
-        {/* Bảo mật thông tin người dùng */}
-        <div></div>
-
-        {/* Cập nhật thường xuyên dựa vào nhu cầu người dùng và xu hướng thế giới */}
-        <div></div>
-
         {/* Đăng kí */}
-        <div></div>
-
         {/* Thắc mắc và liên hệ */}
-        <div></div>
-
+        <div>
+          <div className="flex items-center justify-center h-[30vh] p-6 my-40 border-2 rounded-lg border-blue-400 z-20"
+            data-aos="fade-up"
+            data-aos-duration="2000">
+            <div className="grid grid-flow-row grid-rows-2 text-center">
+              <div className="text-xl font-medium">
+                Ready to experience seamless communication? Join Shockwave now and start connecting instantly!
+              </div>
+              <div className="mt-2">
+                <Button variant="contained">Join Now</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
